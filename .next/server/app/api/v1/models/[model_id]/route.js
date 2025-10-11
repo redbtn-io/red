b4c3650043a36dc78e1341f03005d773f9fea21f
XiroEx <george8794@gmail.com>
2025-10-11 00:37:04 -0400

    removed .next

--- .next/server/app/api/v1/models/[model_id]/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../../../chunks/[turbopack]_runtime.js")("server/app/api/v1/models/[model_id]/route.js")
-R.c("server/chunks/[root-of-the-server]__59574fda._.js")
-R.c("server/chunks/[root-of-the-server]__6b996b85._.js")
-R.c("server/chunks/node_modules_next_48b9435f._.js")
-R.m(10561)
-R.m(54149)
-module.exports=R.m(54149).exports
